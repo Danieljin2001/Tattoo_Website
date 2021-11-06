03067d2941507e442ea0b3f1a4dcb60cf118673e
Daniel Jin <djin2001@gmail.com>
2021-11-06 17:11:12 -0600

    connection to mongoDb established

--- server/index.js
+++ server/index.js
@@ -4,9 +4,24 @@ const dotenv = require("dotenv"); //importing dotenv (hiding pw, users, etc)
 const helmet = require("helmet"); //importing helmet (used to secure requests, as in more security)
 const morgan = require("morgan"); //importing morgan (when a request is made to the server, morgan tells us about the request)
 
-
-
 const app =express();
+
+dotenv.config();
+
+//connection to MongoDB database
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true}, ()=>{
+    console.log("Connected to MongoDB");
+});
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+//test homepage
+app.get("/", (req, res) => {
+    res.send("Welcome to Homepage!");
+});
 
 //port 8800
 app.listen(8800, () =>{
